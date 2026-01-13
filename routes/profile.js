6a7987e79c942b17e12abc671151230430850804
Vikash <vk6484412@gmail.com>
2026-01-13 08:27:06 +0530

    forgot password api added

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const { authUser } = require("../middlewares/auth");
+const User = require("../src/models/user");
 const profileRouter = express.Router();
 
+//view profile
 profileRouter.get("/view", authUser, async (req, res) => {
   try {
     const user = req.user;
@@ -12,14 +15,75 @@ profileRouter.get("/view", authUser, async (req, res) => {
   }
 });
 
-profileRouter.patch("/edit", authUser, async (req, res) => {
+// udpate profile
+profileRouter.patch("/update", authUser, async (req, res) => {
+  const userId = req.user._id;
+  const data = req.body;
+
   try {
-    const user = req.user;
-    res.send("edit user profile pending");
-  } catch (error) {
-    console.error("Error", error.message);
-    res.status(500).send("ERROR: " + error.message);
+    //only below fields are allowd for update
+    const FIELDS_ALLOWED_FOR_UPDATE = [
+      "age",
+      "gender",
+      "photoUrl",
+      "about",
+      "skills",
+      "email",
+    ];
+
+    const isAllowed = Object.keys(data).every((k) =>
+      FIELDS_ALLOWED_FOR_UPDATE.includes(k)
+    );
+
+    if (!isAllowed) {
+      throw new Error("Update not allowed.");
+    }
+
+    const updatedUser = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnreturnDocument: "after",
+      runValidators: true, // Allow custome validator run on update user as well
+    });
+    res.send("User update successfully!");
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Unexpected Error Occured!" + err.message);
   }
 });
 
+//forgot password
+profileRouter.patch("/password", authUser, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    //take old password, newpassword
+    const { oldPassword, newPassword } = req.body;
+
+    //verify the old password
+    const isValid = await loggedInUser.validateUser(oldPassword);
+    if (!isValid) {
+      return res.status(400).json({ message: "Invalid credentials." });
+    }
+    const newHashPassword = await bcrypt.hash(newPassword, 10);
+
+    //if true --> decrpt the new pass and save
+    loggedInUser.password = newHashPassword;
+    await loggedInUser.save();
+    res.send({ message: "Password update successfully." });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Unexpected Error Occured!" + err.message);
+  }
+});
+
+//delete profle
+profileRouter.delete("/delete", authUser, async (req, res) => {
+  const userId = req.user._id;
+  try {
+    //you can use findByIdAndDelete({_id : userId}) as well
+    await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully.");
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Unexpected Error Occured!");
+  }
+});
 module.exports = profileRouter;
--- routes/user.js
+++ routes/user.js
@@ -62,7 +62,6 @@ userRouter.get("/connections", authUser, async (req, res) => {
 //Feed api
 userRouter.get("/feed", authUser, async (req, res) => {
   try {
-
     const loggedInUser = req.user;
     //find out all connection documents where i am either a connecton sender or receiver
     const connections = await ConnectionRequest.find({
@@ -90,83 +89,6 @@ userRouter.get("/feed", authUser, async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).send("ERROR: " + err.message);
-  }
-});
-
-//get user by id
-userRouter.get("/me", authUser, async (req, res) => {
-  try {
-    const user = req.user;
-    res.send(user);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Unexpected Error Occured!");
-  }
-});
-
-//feed --> get all users
-userRouter.get("/feed", authUser, async (req, res) => {
-  try {
-    const users = await User.find({});
-    if (users.length === 0) {
-      res.send("There are no users!");
-    } else {
-      res.send(users);
-    }
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Unexpected Error Occured!");
-  }
-});
-
-//update user
-/**
- * API level senetzation:- you can senetize your each fields at the route handlers
- */
-userRouter.patch("/update-user", authUser, async (req, res) => {
-  const userId = req.user._id;
-  const data = req.body;
-
-  try {
-    //only below fields are allowd for update
-    const FIELDS_ALLOWED_FOR_UPDATE = [
-      "age",
-      "gender",
-      "photoUrl",
-      "about",
-      "skills",
-      "email",
-    ];
-
-    const isAllowed = Object.keys(data).every((k) =>
-      FIELDS_ALLOWED_FOR_UPDATE.includes(k)
-    );
-
-    if (!isAllowed) {
-      throw new Error("Update not allowed.");
-    }
-
-    const updatedUser = await User.findByIdAndUpdate({ _id: userId }, data, {
-      returnreturnDocument: "after",
-      runValidators: true, // Allow custome validator run on update user as well
-    });
-    res.send("User update successfully!");
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Unexpected Error Occured!" + err.message);
-  }
-});
-
-//delete user
-userRouter.delete("/delete-user", authUser, async (req, res) => {
-  const userId = req.user._id;
-  try {
-    //you can use findByIdAndDelete({_id : userId}) as well
-    await User.findByIdAndDelete(userId);
-    res.send("User deleted successfully.");
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Unexpected Error Occured!");
   }
 });
 
