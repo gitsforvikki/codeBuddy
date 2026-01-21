c5ce7cd37eaa2e4b1172cdd19869b5bac35469b0
Vikash <vk6484412@gmail.com>
2026-01-21 16:50:24 +0530

    minor changes

--- utils/cronJob.js
+++ utils/cronJob.js
@@ -1,5 +1,6 @@
 const cron = require("node-cron");
 
-cron.schedule("* * * * *", () => {
+//run at 8 am every days
+cron.schedule("0 8 * * *", () => {
   console.log("running a task every minute");
 });
